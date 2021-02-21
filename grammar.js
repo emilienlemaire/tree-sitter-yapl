1cd164eb3e2fb0de228bed1cc0ff452db7b23579
Emilien Lemaire <emilien.lem@icloud.com>
2021-02-21 19:56:27 +0100

    Add function call

--- grammar.js
+++ grammar.js
@@ -10,7 +10,7 @@ module.exports = grammar({
             $.declaration_statement,
             $.initialization_statement,
             //$.struct_definition,
-            //$.function_defintion
+            $.function_definition
         )),
 
         _declaration: $ => choice(
@@ -24,13 +24,48 @@ module.exports = grammar({
             $.struct_initialization
         ),
 
-        _values: $ => choice(
+        _expression: $ => choice(
             $.identifier,
-            $.number,
+            $._numbers,
             $.bool_literal,
-            //$.array_access,
-            //$.attribute_access
-            //$.function_call
+            $.array_access,
+            $.attribute_access,
+            $.function_call,
+            $._binary_expression,
+            $._par_expression
+        ),
+
+        _par_expression: $ => seq(
+            '(',
+            $._expression,
+            ')'
+        ),
+
+        _numbers: $ => choice(
+            $.int_literal,
+            $.float_literal,
+            $.double_literal
+        ),
+
+        _callable: $ => choice(
+            $.identifier,
+            $.attribute_access
+        ),
+
+        _binary_operator: $ => choice(
+            $.plus,
+            $.minus,
+            $.times,
+            $.by,
+            $.mod,
+            $.lth,
+            $.mth,
+            $.eq,
+            $.leq,
+            $.meq,
+            $.neq,
+            $.or,
+            $.and
         ),
 
         declaration_statement: $ => seq(
@@ -43,6 +78,21 @@ module.exports = grammar({
             $.semi
         ),
 
+        function_definition: $ => seq(
+            'func',
+            field('name', $.identifier),
+            field('parameters', $.parameters),
+            '->',
+            field('type', $.identifier),
+            field('body', $.block)
+        ),
+
+        block: $ => seq(
+            '{',
+            repeat($.declaration_statement),
+            '}'
+        ),
+
         simple_declaration: $ => seq(
             field('type', $.identifier),
             field('name', $.identifier)
@@ -51,54 +101,213 @@ module.exports = grammar({
         array_declaration: $ => seq(
             $.simple_declaration,
             '[',
-            field('size', $.number),
+            field('size', $.int_literal),
             ']'
         ),
 
-        simple_initialization: $ => seq(
+        simple_initialization: $ => prec.right(16, seq(
             $.simple_declaration,
             '=',
-            field('value', $._values)
-        ),
+            field('value', $._expression)
+        )),
 
-        array_initialization: $ => seq(
+        array_initialization: $ => prec.right(16, seq(
             $.array_declaration,
             '=',
             $.initializer_list
-        ),
+        )),
 
         struct_initialization: $ => choice(
             $.struct_constructor_initialization,
             $.struct_list_initialization
         ),
 
-        struct_constructor_initialization: $ => seq(
+        struct_constructor_initialization: $ => prec.left(2, seq(
             $.simple_declaration,
-            '(',
             field('values', $.arguments),
-            ')'
-        ),
+        )),
 
-        struct_list_initialization: $ => seq(
+        struct_list_initialization: $ => prec.right(16, seq(
             $.simple_declaration,
             '=',
             $.initializer_list
-        ),
+        )),
 
         initializer_list: $ => seq(
             '{',
-            field('values', $.arguments),
+            field('values', optional($.arguments_list)),
             '}'
         ),
 
+        parameters: $ => seq(
+            '(',
+            optional(
+                seq(
+                    $.declaration_statement,
+                    repeat(
+                        seq(
+                            ',',
+                            $.declaration_statement
+                        )
+                    )
+                )
+            ),
+            ')'
+        ),
+
         arguments: $ => seq(
-            $._values,
+            '(',
+            optional(
+                seq(
+                    $._expression,
+                    repeat(
+                        seq(
+                            ',',
+                            $._expression
+                        )
+                    )
+                )
+            ),
+            ')'
+        ),
+
+        arguments_list: $ => seq(
+            $._expression,
             repeat(
                 seq(
                     ',',
-                    $._values
+                    $._expression
                 )
             )
+        ),
+
+
+        _binary_expression: $ => choice(
+            $.add,
+            $.substract,
+            $.multiply,
+            $.divide,
+            $.mod_expr,
+            $.lth_cmp,
+            $.mth_cmp,
+            $.eq_cmp,
+            $.neq_cmp,
+            $.leq_cmp,
+            $.meq_cmp,
+            $.and_expr,
+            $.or_expr
+        ),
+
+        add: $ => prec.left(6, seq(
+            field('lhs', $._expression),
+            $.plus,
+            field('rhs', $._expression)
+        )),
+
+        substract: $ => prec.left(6, seq(
+            field('lhs', $._expression),
+            $.minus,
+            field('rhs', $._expression)
+        )),
+
+        multiply: $ => prec.left(5, seq(
+            field('lhs', $._expression),
+            $.times,
+            field('rhs', $._expression)
+        )),
+
+        divide: $ => prec.left(5, seq(
+            field('lhs', $._expression),
+            $.by,
+            field('rhs', $._expression)
+        )),
+
+        mod_expr: $ => prec.left(5, seq(
+            field('lhs', $._expression),
+            $.mod,
+            field('rhs', $._expression)
+        )),
+
+        lth_cmp: $ => prec.left(9, seq(
+            field('lhs', $._expression),
+            $.lth,
+            field('rhs', $._expression)
+        )),
+
+        mth_cmp: $ => prec.left(9, seq(
+            field('lhs', $._expression),
+            $.mth,
+            field('rhs', $._expression)
+        )),
+
+        eq_cmp: $ => prec.left(10, seq(
+            field('lhs', $._expression),
+            $.eq,
+            field('rhs', $._expression)
+        )),
+
+        neq_cmp: $ => prec.left(10, seq(
+            field('lhs', $._expression),
+            $.neq,
+            field('rhs', $._expression)
+        )),
+
+        leq_cmp: $ => prec.left(9, seq(
+            field('lhs', $._expression),
+            $.leq,
+            field('rhs', $._expression)
+        )),
+
+        meq_cmp: $ => prec.left(9, seq(
+            field('lhs', $._expression),
+            $.meq,
+            field('rhs', $._expression)
+        )),
+
+        and_expr: $ => prec.left(14, seq(
+            field('lhs', $._expression),
+            $.and,
+            field('rhs', $._expression)
+        )),
+
+        or_expr: $ => prec.left(15, seq(
+            field('lhs', $._expression),
+            $.or,
+            field('rhs', $._expression)
+        )),
+
+        array_access: $ => prec.left(2, seq(
+            field('name', $.identifier),
+            '[',
+            field('index', $.int_literal),
+            ']'
+        )),
+
+        attribute_access: $ => prec.left(2, seq(
+            field('struct', $.identifier),
+            '.',
+            field('attribute', $.identifier)
+        )),
+
+        function_call: $ => prec.left(2, seq(
+            field('function', $._callable),
+            field('arguments', $.arguments),
+        )),
+
+        int_literal: $ => $._digits,
+
+        float_literal: $ => seq(
+            optional($._digits),
+            '.',
+            $._digits,
+            'f'
+        ),
+
+        double_literal: $ => seq(
+            optional($._digits),
+            '.',
+            $._digits,
+            optional('d')
         ),
 
         bool_literal: $ => choice(
@@ -108,9 +317,35 @@ module.exports = grammar({
 
         identifier: $ => /([a-zA-Z]|_)+([a-zA-Z0-9]|_)*/,
 
-        number: $ => /\d+/,
+        _digits: $ => /\d+/,
 
-        semi: $ => ';'
+        semi: $ => ';',
+
+        plus: $ => '+',
+
+        minus: $ => '-',
+
+        times: $ => '*',
+
+        by: $ => '/',
+
+        mod: $ => '%',
+
+        lth: $ => '<',
+
+        mth: $ => '>',
+
+        eq: $ => '==',
+
+        leq: $ => '<=',
+
+        meq: $ => '>=',
+
+        neq: $ => '!=',
+
+        or: $ => '||',
+
+        and: $ => '&&'
     }
 });
 
